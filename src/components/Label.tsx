b0c042c9eed6e19b80eff4a89f0f76c65131b184
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-14 09:25:38 +0100

    adding basic components

---
+++ src/components/Label.tsx
@@ -0,0 +1,3 @@
+export default function Label() {
+  return <label>I'm a label</label>;
+}
---
+++ src/components/Radio.tsx
@@ -0,0 +1,8 @@
+export default function Radio() {
+  return (
+    <label>
+      <input type="radio" />
+      I'm a radio
+    </label>
+  );
+}
