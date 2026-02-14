66904582f9b29336c13c5dff89258c930712c54f
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-14 22:57:15 +0100

    improve paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tsconfigPaths()],
 });
