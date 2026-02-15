612d3c81e91d70774c40514827b20ab5b4802b44
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-15 20:30:19 +0100

    starting to add cosmos

---
+++ cosmos-decorator.tsx
@@ -0,0 +1,14 @@
+// Node modules
+import type { ReactNode } from "react";
+
+// Project files
+import "styles/style.css";
+import "styles/cosmos-style.css"; // To center tested components on screen
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function decorator({ children }: Props) {
+  return <div id="cosmos">{children}</div>;
+}
---
+++ src/components/button/Button.fixture.tsx
@@ -0,0 +1,3 @@
+import Button from "./Button";
+
+export default <Button onClick={() => alert("clicked me")}>Click me</Button>;
