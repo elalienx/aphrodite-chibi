ce0a046f02d6a0e839af91078a0015b4844053ef
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-15 18:27:22 +0100

    refactoring css

--- src/ui/form/Step1.tsx
+++ src/ui/form/Step1.tsx
@@ -3,26 +3,35 @@ import Input from "components/Input";
 import Label from "components/Label";
 
 import "styles/style.css";
+import "./step-1.css";
 
 export default function Step1() {
   return (
     <div id="step-1" className="soft-background">
-      <h2>Step 1</h2>
+      <header>
+        <h2>Step 1</h2>
+      </header>
 
-      {/* Name */}
-      <Label>Name</Label>
-      <Input />
+      <section>
+        {/* Name */}
+        <Label>Name</Label>
+        <Input />
 
-      {/* Email */}
-      <Label>Email</Label>
-      <Input />
+        {/* Email */}
+        <Label>Email</Label>
+        <Input />
 
-      {/* Phone */}
-      <Label>Phone</Label>
-      <Input />
+        {/* Phone */}
+        <Label>Phone</Label>
+        <Input />
+      </section>
+
+      <hr />
 
       {/* Submit */}
-      <Button>Next</Button>
+      <footer>
+        <Button>Next</Button>
+      </footer>
     </div>
   );
 }
