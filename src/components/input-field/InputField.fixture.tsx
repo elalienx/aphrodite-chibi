197eadcd85e159a1f06d3e4004502efade30790c
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-16 21:16:45 +0100

    very basic implementation of input field CSS

--- src/components/input-field/InputField.fixture.tsx
+++ src/components/input-field/InputField.fixture.tsx
@@ -1,3 +1,10 @@
+import Label from "../label/Label";
+import Input from "../input/Input";
 import InputField from "./InputField";
 
-export default <InputField />;
+export default (
+  <InputField>
+    <Label>Har du delad vårdnad?</Label>
+    <Input />
+  </InputField>
+);
--- src/components/input-field/InputField.tsx
+++ src/components/input-field/InputField.tsx
@@ -1,3 +1,21 @@
-export default function InputField() {
-  return <div>InputField</div>;
+import type { ReactNode } from "react";
+
+import "./input-field.css";
+
+interface Props {
+  /** Text to display inside the alert. */
+  children?: ReactNode;
+}
+
+export default function InputField({ children }: Props) {
+  // Safeguard
+  if (!children)
+    return (
+      <p>
+        Please add an <code>Label</code> and a <code>InputField</code> to get
+        started
+      </p>
+    );
+
+  return <div className="input-field">{children}</div>;
 }
