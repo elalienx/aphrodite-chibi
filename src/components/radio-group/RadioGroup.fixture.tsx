2b2a6231b25ef6aaf1c98b828297fa791f6ef2be
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-16 21:40:19 +0100

    styling radio group

--- src/components/radio-group/RadioGroup.fixture.tsx
+++ src/components/radio-group/RadioGroup.fixture.tsx
@@ -1,3 +1,11 @@
+import RadioOption from "../radio-option/RadioOption";
+import Label from "../label/Label";
 import RadioGroup from "./RadioGroup";
 
-export default <RadioGroup />;
+export default (
+  <RadioGroup>
+    <Label>Har du delad vårdnad?</Label>
+    <RadioOption>Ja</RadioOption>
+    <RadioOption>Nej</RadioOption>
+  </RadioGroup>
+);
--- src/components/radio-group/RadioGroup.tsx
+++ src/components/radio-group/RadioGroup.tsx
@@ -1,3 +1,38 @@
-export default function RadioGroup() {
-  return <div>RadioGroup</div>;
+import React from "react";
+
+import RadioOption from "../radio-option/RadioOption";
+import Label from "../label/Label";
+import "./radio-group.css";
+
+interface Props {
+  /** Text to display inside the alert. */
+  children?: React.ReactNode;
+}
+
+export default function RadioGroup({ children }: Props) {
+  // Safeguard
+  if (!children)
+    return (
+      <p>
+        Please add an <code>Label</code> and at least two{" "}
+        <code>RadioOption</code> to get started
+      </p>
+    );
+
+  const childArray = React.Children.toArray(children);
+
+  const label = childArray.find(
+    (child) => React.isValidElement(child) && child.type === Label,
+  );
+
+  const options = childArray.filter(
+    (child) => React.isValidElement(child) && child.type === RadioOption,
+  );
+
+  return (
+    <div className="radio-group">
+      {label}
+      <div className="radio-options">{options}</div>
+    </div>
+  );
 }
