8c82a61b2319acdb4ae64f5d4a54b8999d8a412e
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-18 23:07:32 +0100

    adding placeholder

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,5 +1,12 @@
 import "./input.css";
 
-export default function Input() {
-  return <input className="input" type="text" placeholder="I'm an input" />;
+interface Props {
+  /** An example value to show when the field is empty. */
+  placeholder?: string;
+  /** Decides what kind of keyboard to show on mobile. This does not affect validation. Handle that separately. */
+  type: "text" | "number" | "email" | "tel" | "password";
+}
+
+export default function Input({ placeholder = "", type = "text" }: Props) {
+  return <input className="input" type={type} placeholder={placeholder} />;
 }
--- src/cosmos/components/Input.fixture.tsx
+++ src/cosmos/components/Input.fixture.tsx
@@ -1,3 +1,9 @@
 import Input from "../../components/input/Input";
 
-export default <Input />;
+export default {
+  text: <Input type="text" placeholder="I'm an input" />,
+  number: <Input type="number" placeholder="0" />,
+  email: <Input type="email" placeholder="leif@lendo.se" />,
+  telephone: <Input type="tel" placeholder="+46 729478013" />,
+  password: <Input type="password" placeholder="Minimum 8 characters" />,
+};
