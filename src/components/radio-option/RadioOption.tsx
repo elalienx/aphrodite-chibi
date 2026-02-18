579a913c62495f3ab40d902f2a3120051af10587
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-18 23:17:03 +0100

    foo

--- src/components/radio-option/RadioOption.tsx
+++ src/components/radio-option/RadioOption.tsx
@@ -5,12 +5,15 @@ import "./radio-option.css";
 interface Props {
   /** Text to display inside the radio option. */
   label: string;
+
+  /** Unique identifier of the parent radio group to make sure only one radio option is active. */
+  id: string;
 }
 
-export default function RadioOption({ label }: Props) {
+export default function RadioOption({ label, id }: Props) {
   return (
     <label className="radio-option">
-      <input type="radio" />
+      <input type="radio" name={id} />
       {label}
     </label>
   );
