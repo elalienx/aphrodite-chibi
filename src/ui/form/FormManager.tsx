5773607c714377caae97bf3406ee7b22becfa840
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-19 03:12:12 +0100

    adding sfix

--- src/ui/form/FormManager.tsx
+++ src/ui/form/FormManager.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
+import Step3 from "./Step3";
 import Success from "./Success";
 
-type Step = "step1" | "step2" | "success";
+type Step = "step1" | "step2" | "step3" | "success";
 
 export default function FormManager() {
   const [step, setStep] = useState<Step>("step1");
@@ -11,7 +12,8 @@ export default function FormManager() {
   return (
     <div>
       {step === "step1" && <Step1 onSubmit={() => setStep("step2")} />}
-      {step === "step2" && <Step2 onSubmit={() => setStep("success")} />}
+      {step === "step2" && <Step2 onSubmit={() => setStep("step3")} />}
+      {step === "step3" && <Step3 onSubmit={() => setStep("success")} />}
       {step === "success" && <Success onSubmit={() => setStep("step1")} />}
     </div>
   );
---
+++ src/ui/form/Step3.tsx
@@ -0,0 +1,41 @@
+import Button from "components/button/Button";
+import InputField from "components/input-field/InputField";
+import Input from "components/input/Input";
+import Label from "components/label/Label";
+import RadioGroup from "components/radio-group/RadioGroup";
+import RadioOption from "components/radio-option/RadioOption";
+
+interface Props {
+  onSubmit: () => void;
+}
+
+export default function Step3({ onSubmit }: Props) {
+  return (
+    <div className="soft-background">
+      <section className="top">
+        <h2>Step 3</h2>
+
+        <RadioGroup id="PEP">
+          <Label>PEP</Label>
+          <RadioOption>Ja</RadioOption>
+          <RadioOption>Nej</RadioOption>
+          <RadioOption>Maybe?</RadioOption>
+        </RadioGroup>
+
+        <InputField>
+          <Label>Age</Label>
+          <Input type="text" placeholder="foo" />
+        </InputField>
+
+        <InputField>
+          <Label>Phone</Label>
+          <Input type="text" placeholder="foo" />
+        </InputField>
+      </section>
+      <hr />
+      <section className="bottom">
+        <Button onClick={onSubmit}>Foobar</Button>
+      </section>
+    </div>
+  );
+}
