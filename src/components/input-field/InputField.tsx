5d166f143424ee2ea3b433878d80f1fb8454165e
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-02-21 15:47:44 +0100

    ddd

--- src/components/input-field/InputField.tsx
+++ src/components/input-field/InputField.tsx
@@ -5,17 +5,25 @@ import "./input-field.css";
 interface Props {
   /**  Content to display inside the input field. */
   children?: ReactNode;
+
+  /** Information shown to the user when the validatio runs. */
+  validationMessage?: string;
 }
 
-export default function InputField({ children }: Props) {
+export default function InputField({ children, validationMessage }: Props) {
   // Safeguard
   if (!children)
     return (
       <p>
-        Please add an <code>Label</code> and a <code>InputField</code> to get
-        started
+        Please add an <code>Label</code> and a <code>InputField</code> to get started
       </p>
     );
 
-  return <div className="input-field">{children}</div>;
+  return (
+    <div className="input-field">
+      {children}
+
+      {validationMessage && <p className="validation-message">{validationMessage}</p>}
+    </div>
+  );
 }
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -6,15 +6,9 @@ interface Props {
 
   /** Decides what kind of keyboard to show on mobile. This does not affect validation. Handle that separately. */
   type: "text" | "number" | "email" | "tel" | "password";
-
-  /**  */
-  validationSatus?: "clear" | "info" | "warnig" | "error";
-
-  /**  */
-  validationMessage?: string;
 }
 
-export default function Input({ placeholder, type, validationSatus, validationMessage }: Props) {
+export default function Input({ placeholder, type }: Props) {
   // Properties
   const mobileKeyboard = getMobileKeyboard(type);
 
@@ -37,8 +31,6 @@ export default function Input({ placeholder, type, validationSatus, validationMe
   return (
     <>
       <input className="input" type={type} inputMode={mobileKeyboard} placeholder={placeholder} />
-
-      {validationMessage && <p className="validation-message">{validationMessage}</p>}
     </>
   );
 }
