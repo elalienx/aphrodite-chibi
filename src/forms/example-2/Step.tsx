4f52885352430db6a3de142199189f3d6f81e97a
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-02-21 14:20:06 +0100

    adding example 2

---
+++ src/forms/example-2/Step.tsx
@@ -0,0 +1,35 @@
+import Button from "components/button/Button";
+import Input from "components/input/Input";
+import InputField from "components/input-field/InputField";
+import Label from "components/label/Label";
+
+export default function Step() {
+  return (
+    <div className="soft-background">
+      <section className="top">
+        <h2 className="level-4">Step 1</h2>
+
+        <InputField>
+          <Label>Namn och efternamn</Label>
+          <Input type="text" placeholder="Leif Lend" />
+        </InputField>
+
+        <InputField>
+          <Label>E-postadress</Label>
+          <Input type="email" placeholder="leif@lendo.se" />
+        </InputField>
+
+        <InputField>
+          <Label>Telefonnummer</Label>
+          <Input type="tel" placeholder="+46 729478013" />
+        </InputField>
+      </section>
+
+      <hr />
+
+      <section className="bottom">
+        <Button>Nästa</Button>
+      </section>
+    </div>
+  );
+}
