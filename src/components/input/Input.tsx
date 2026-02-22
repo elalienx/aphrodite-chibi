f61bc608399b0f4c20da169d423a89afb993e16c
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-22 19:05:55 +0100

    polish

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,6 +3,7 @@ import getCorrectMobileKeyboard from "helpers/getCorrectMobileKeyboard";
 import "./input-wrapper-layout.css";
 import "./input-wrapper-state.css";
 import "./input-wrapper-design.css";
+import "./input-type-number.css";
 
 interface Props {
   /** An example value to show when the field is empty. */
