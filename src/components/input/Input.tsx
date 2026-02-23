41e1640e2c5ab5360b420805b09613397fcda396
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-23 17:40:23 +0100

    adding valibot

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,3 +1,5 @@
+import { useField, type FormStore } from "@formisch/react";
+
 import getCorrectMobileKeyboard from "helpers/getCorrectMobileKeyboard";
 
 import "./input-wrapper-layout.css";
@@ -6,6 +8,9 @@ import "./input-wrapper-design.css";
 import "./input-type-number.css";
 
 interface Props {
+  form: FormStore;
+  id: string;
+
   /** An example value to show when the field is empty. */
   placeholder?: string;
 
@@ -19,17 +24,29 @@ interface Props {
   validationMessage?: string;
 }
 
-export default function Input({ placeholder, type, validationMessage, suffix }: Props) {
+export default function Input({ form, id, placeholder, type, suffix, validationMessage }: Props) {
+  // Local state
+  const field = useField(form, { path: [id] });
+
   // Properties
+  const fieldValue = typeof field.input === "string" ? field.input : "";
+  const firstError = field.errors?.[0];
   const mobileKeyboard = getCorrectMobileKeyboard(type);
   const cssSuffix = suffix ? "has-suffix" : "";
-  const cssValidationMessage = validationMessage ? "has-validation-message" : "";
+  const cssValidationMessage = firstError ? "has-validation-message" : "";
 
   return (
     <div className={`input-wrapper ${cssSuffix} ${cssValidationMessage}`}>
-      <input className="input" type={type} inputMode={mobileKeyboard} placeholder={placeholder} />
+      <input
+        {...field.props}
+        className="input"
+        inputMode={mobileKeyboard}
+        placeholder={placeholder}
+        type={type}
+        value={fieldValue}
+      />
       {suffix && <span className="suffix">{suffix}</span>}
-      {validationMessage && <p className="validation-message">{validationMessage}</p>}
+      {firstError && <p className="validation-message">{firstError}</p>}
     </div>
   );
 }
--- src/forms/example-1/Step1.tsx
+++ src/forms/example-1/Step1.tsx
@@ -6,35 +6,41 @@ import Input from "components/input/Input";
 import InputField from "components/input-field/InputField";
 import Label from "components/label/Label";
 
-interface Props {
-  onSubmit: () => void;
-}
-
 const Step1Schema = v.object({
-  name: v.pipe(v.string(), v.minLength(10)),
+  name: v.pipe(v.string(), v.minLength(5)),
+  email: v.pipe(v.string(), v.email()),
 });
 
-export default function Step1({ onSubmit }: Props) {
+export default function Step1() {
   // Properties
-  const step1Form = useForm({ schema: Step1Schema });
+  const form = useForm({ schema: Step1Schema });
+
+  // Methods
+  function onSubmit() {
+    // check if the form is valid
+    // then run the function send by the parent
+  }
 
   return (
-    <Form className="soft-background" of={step1Form} onSubmit={(output, event) => console.log(output)}>
+    <Form className="soft-background" of={form} onSubmit={(output, event) => console.log(output)}>
       <section className="top">
         <h2 className="level-4">Step 1</h2>
 
         <InputField>
           <Label>Namn och efternamn</Label>
-          <Input type="text" placeholder="Leif Lend" />
+          <Input form={form} id="name" type="text" placeholder="Leif Lend" />
+        </InputField>
+
+        <InputField>
+          <Label>Namn och efternamn</Label>
+          <Input form={form} id="email" type="text" placeholder="leif@lendo.se" />
         </InputField>
       </section>
 
       <hr />
 
       <section className="bottom">
-        <Button onClick={onSubmit} type="submit">
-          Nästa
-        </Button>
+        <Button type="submit">Nästa</Button>
       </section>
     </Form>
   );
