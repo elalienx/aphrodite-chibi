7dfa8d319024ccd0a3c91d568b27645c2badfda1
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-24 23:45:29 +0100

    foobar

--- src/forms/example-1/Step1.tsx
+++ src/forms/example-1/Step1.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 const schema = v.object({
-  name: v.pipe(v.string("Please enter your name."), v.minLength(5, "This name is too short")),
+  name: v.pipe(v.string("Please enter your name."), v.minLength(3, "This name is too short")),
   email: v.pipe(v.string("Please enter your email."), v.email("This email is not valid")),
 });
 
