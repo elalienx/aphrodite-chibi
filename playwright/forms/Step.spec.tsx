4dbd82d7bc2bfc316e859fc6a3837bac142ecf03
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-27 00:33:22 +0100

    test

---
+++ playwright/forms/Step.spec.tsx
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/experimental-ct-react";
+import Step1 from "forms/example-1/Step1";
+
+test("should work", async ({ mount }) => {
+  const component = await mount(<Step1 onContinue={() => alert("submitted")} />);
+  await expect(component).toContainText("Learn React");
+});
