c1c35bbee5bea1cb8445fe18f2b4f6a5b0bee8f6
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-02-28 22:02:38 +0100

    adding back step mvp

--- src/forms/example-2/Step.tsx
+++ src/forms/example-2/Step.tsx
@@ -1,1 +1,40 @@
-sss
+import Button from "components/button/Button";
+import Input from "components/input/Input";
+import InputField from "components/input-field/InputField";
+import Label from "components/label/Label";
+
+import "./step.css";
+
+export default function Step() {
+  return (
+    <div id="step" className="soft-background">
+      <section className="top">
+        <header>
+          <a href="/">Tillbaka</a>
+          <h2 className="level-4">2. Om bostaden</h2>
+        </header>
+
+        <InputField>
+          <Label>Kvadratmeter</Label>
+          <Input type="number" placeholder="0" suffix="kvm" />
+        </InputField>
+
+        <InputField>
+          <Label>Antal rum</Label>
+          <Input type="number" placeholder="0" suffix="st" />
+        </InputField>
+
+        <InputField>
+          <Label>Månadsavgift</Label>
+          <Input type="number" placeholder="0" suffix="kr/mån" />
+        </InputField>
+      </section>
+
+      <hr />
+
+      <section className="bottom">
+        <Button>Nästa</Button>
+      </section>
+    </div>
+  );
+}
