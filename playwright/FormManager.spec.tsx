3754e2deaeee92717277782fd743d9a735eb7ad6
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-03-01 18:05:42 +0100

    starting test

--- playwright/FormManager.spec.tsx
+++ playwright/FormManager.spec.tsx
@@ -2,8 +2,14 @@ import { test, expect } from "@playwright/experimental-ct-react";
 
 import FormManager from "../src/forms/example-3/FormManager";
 
-test("should work", async ({ mount }) => {
+test("Should show error on both field when pressing submit", async ({ mount }) => {
   const component = await mount(<FormManager />);
 
-  await expect(component).toContainText("Playwright test");
+  await component.getByRole("button", { name: "Nästa" }).click();
+
+  // Input 1
+
+  // Input 2
+
+  await expect(component.getByRole("heading", { name: "Playwright test" })).toBeVisible();
 });
