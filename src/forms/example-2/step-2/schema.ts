7dc80e3dd762dd3171a3fb1adbbdc57e797f2457
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-03-17 22:07:05 +0100

    fix

--- src/forms/example-2/step-2/schema.ts
+++ src/forms/example-2/step-2/schema.ts
@@ -3,6 +3,7 @@ import * as v from "valibot";
 export const schema = v.object({
   size: v.pipe(
     v.string("Ange bostadens månadsavgift för att gå vidare."),
+    v.nonEmpty("Ange bostadens månadsavgift för att gå vidare."),
     v.transform((value) => Number(value)),
     v.number("Must be a valid number."),
     v.minValue(1, "Ange bostadens månadsavgift för att gå vidare."),
@@ -10,13 +11,15 @@ export const schema = v.object({
   ),
   rooms: v.pipe(
     v.string("Ange hur många rum bostaden har för att gå vidare."),
+    v.nonEmpty("Ange hur många rum bostaden har för att gå vidare."),
     v.transform((value) => Number(value)),
     v.number("Must be a valid number."),
     v.minValue(1, "Du har angivit för få rum. Du kan som lägst ange 1 rum."),
     v.maxValue(10, "Du har angivit för många rum. Du kan som högst ange 10 rum."),
   ),
   monthly_fee: v.pipe(
-    v.string("Ange bostadens månadsavgift för att gå vidare"),
+    v.string("Ange bostadens månadsavgift för att gå vidare."),
+    v.nonEmpty("Ange bostadens månadsavgift för att gå vidare."),
     v.transform((value) => Number(value)),
     v.number("Must be a valid number"),
     v.minValue(1, "Ange bostadens månadsavgift för att gå vidare."),
