5360e4be9ceeb4123a3405f77cb2f6c563b36a5e
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-03-19 20:00:37 +0100

    adding design

--- src/components/tooltip/OpenDesign.tsx
+++ src/components/tooltip/OpenDesign.tsx
@@ -1,7 +1,18 @@
+import "./open-design.css";
+
+/**
+ * About:
+ * This is the experimental component to design a tooltip with a pointy arrow.
+ *
+ * Approaches:
+ * - Native CSS shape with clip path.
+ * - Have a pre-stylized SVG with boder and shadown that can be positioned and rotated to each edge.
+ */
 export default function OpenDesign() {
   return (
     <div className="open-design">
-      <div className="window">Im the best tooltip 🥇</div>
+      <div className="window soft-shadow">Im the best tooltip 🥇</div>
+      <div className="arrow">{/* empty on purpose */}</div>
     </div>
   );
 }
