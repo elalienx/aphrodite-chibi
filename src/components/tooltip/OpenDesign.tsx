f1d341c037dae6100798500d332e65e0f0f125b2
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-03-20 16:43:47 +0100

    adding code

--- src/components/tooltip/OpenDesign.tsx
+++ src/components/tooltip/OpenDesign.tsx
@@ -1,16 +1,12 @@
 import "./open-design.css";
 
-/**
- * About:
- * This is the experimental component to design a tooltip with a pointy arrow.
- *
- * Approaches:
- * - Native CSS shape with clip path.
- * - Have a pre-stylized SVG with boder and shadown that can be positioned and rotated to each edge.
- */
-export default function TooltipPlus() {
+interface Props {
+  direction: "top" | "left" | "right" | "bottom";
+}
+
+export default function TooltipPlus({ direction = "top" }: Props) {
   return (
-    <div className="tool-tip-plus">
+    <div className={`tool-tip-plus ${direction}`}>
       <div className="window soft-shadow">Im the best tooltip in pug world 🥇</div>
       <div className="arrow ">{/* empty on purpose */}</div>
     </div>
