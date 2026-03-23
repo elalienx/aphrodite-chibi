8b8878e8f4c8b91a64fbd9d795112277b1214d44
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-03-23 21:37:31 +0100

    refactor

--- src/forms/example-2/success/Success.tsx
+++ src/forms/example-2/success/Success.tsx
@@ -9,26 +9,17 @@ export default function Success({ onContinue }: Props) {
     <div className="soft-background">
       <section className="top">
         <header>
-          <h3>Tack för din ansökan!</h3>
+          <h3>Demo complete!</h3>
         </header>
 
-        <section className="soft-green-background">
-          <header>
-            <h4>Vad händer nu?</h4>
-          </header>
-          <b>Vi ringer dig!</b>
-          <p>En handläggare från Lendo kommer att ringa upp dig inom kort för att gå igenom din bolåneansökan.</p>
-          <br />
-          <p>
-            Det är viktigt att du svarar när vi ringer, eftersom vi behöver prata med dig innan vi kan gå vidare med
-            eventuella erbjudanden.
-          </p>
-          <br />
-          <b>Vill du hellre ringa oss direkt?</b>
-          <p>
-            Kontakta oss på <a href="tel:+46771190190">0771 - 190 190</a>.
-          </p>
-        </section>
+        <p>
+          Please send your feedback on what parts of the previous screen did not
+          behave like the real Lendo site.
+        </p>
+        <p>
+          Feel free to critizise everything from missing icons, incorrect
+          spacing, font size, etc.
+        </p>
       </section>
 
       <hr />
