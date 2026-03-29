2d9aebf871b0a2940220bb0b3d9b651c061b75da
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-03-29 20:40:59 +0200

    finish radio button tests (next step refactor)

--- playwright/RadioGroup.spec.tsx
+++ playwright/RadioGroup.spec.tsx
@@ -5,17 +5,17 @@ import type { MountResult } from "@playwright/experimental-ct-react";
 
 import FormManager from "../src/forms/example-3/FormManager";
 
-const aria2 = "#aria-error-likes_beer";
-const aria3 = "#aria-error-likes_guiness";
-const specificError = "What property you will purchase";
-const genericError = "Say either yes or no.";
-
 let component: MountResult;
+let errorRadio1: Locator;
+let errorRadio2: Locator;
 let cleanUpText: Locator;
 let submitButton: Locator;
 
 test.beforeEach(async ({ mount }) => {
   component = await mount(<FormManager />);
+
+  errorRadio1 = component.locator("#aria-error-likes_beer");
+  errorRadio2 = component.locator("#aria-error-likes_guiness");
   cleanUpText = component.getByText("Text to clean Playwright selector");
   submitButton = component.getByRole("button", { name: "Next" });
 });
@@ -29,18 +29,38 @@ test("1. Should show error state when submitting empty form", async () => {
   await submitButton.click();
 
   // Assert
-  await expect(component.getByText(specificError)).toBeVisible();
-  await expect(component.locator(aria2).getByText(genericError)).toBeVisible();
-  await expect(component.locator(aria3).getByText(genericError)).toBeVisible();
+  await expect(errorRadio1).toBeVisible();
+  await expect(errorRadio2).toBeVisible();
 });
 
 test("2. Should submit successfully", async () => {
   // Act
-  await component.getByText("House", { exact: true }).click();
   await component.getByText("Yes").first().click();
   await component.getByText("Yes").nth(1).click();
   await submitButton.click();
 
   // Assert
-  await expect(component.getByText("Succes")).toBeVisible();
+  await expect(errorRadio1).not.toBeVisible();
+  await expect(errorRadio2).not.toBeVisible();
+});
+
+test("3. Clicking on a radio button with error should immediately remove the error", async () => {
+  await test.step("Trigger error", async () => {
+    // Act
+    await component.getByText("Yes").first().click();
+    await submitButton.click();
+
+    // Assert
+    await expect(errorRadio1).not.toBeVisible();
+    await expect(errorRadio2).toBeVisible();
+  });
+
+  await test.step("Clear error", async () => {
+    // Act
+    await component.getByText("Yes").nth(1).click();
+
+    // Assert
+    await expect(errorRadio1).not.toBeVisible();
+    await expect(errorRadio2).not.toBeVisible();
+  });
 });
