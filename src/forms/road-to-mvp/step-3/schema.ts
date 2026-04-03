c9b1c6289ef9d5c7df1cc8845cafab29b3fdcd95
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-04-04 01:29:07 +0200

    basic code

--- src/forms/road-to-mvp/step-3/schema.ts
+++ src/forms/road-to-mvp/step-3/schema.ts
@@ -1,51 +1,44 @@
 import * as v from "valibot";
 
-const schema = v.intersect([
-  v.object({
-    size: v.pipe(
-      v.string("Ange bostadens yta för att gå vidare."),
-      v.nonEmpty("Ange bostadens yta för att gå vidare."),
-      v.transform((value) => Number(value)),
-      v.number("Must be a valid number."),
-      v.minValue(1, "Boytan är för liten."),
-      v.maxValue(2000, "Boytan är för stor. Max 2000 kvm."),
-    ),
-    rooms: v.pipe(
-      v.string("Ange hur många rum bostaden har för att gå vidare."),
-      v.nonEmpty("Ange hur många rum bostaden har för att gå vidare."),
-      v.transform((value) => Number(value)),
-      v.number("Must be a valid number."),
-      v.minValue(1, "Du kan som lägst ange 1 rum."),
-      v.maxValue(10, "Du kan som högst ange 10 rum."),
-    ),
-    property_type: v.picklist(["house", "apartment", "terraced_house", "holiday_home"]),
-  }),
+export const size = v.pipe(
+  v.string("Ange bostadens yta för att gå vidare."),
+  v.nonEmpty("Ange bostadens yta för att gå vidare."),
+  v.transform((value) => Number(value)),
+  v.number("Must be a valid number."),
+  v.minValue(1, "Boytan är för liten."),
+  v.maxValue(2000, "Boytan är för stor. Max 2000 kvm."),
+);
 
-  // Conditional fields based on property_type
-  v.variant("property_type", [
-    v.object({
-      property_type: v.literal("apartment"),
-      monthly_fee: v.pipe(
-        v.string("Ange bostadens månadsavgift för att gå vidare."),
-        v.nonEmpty("Ange bostadens månadsavgift för att gå vidare."),
-        v.transform((value) => Number(value)),
-        v.number("Must be a valid number"),
-        v.minValue(1, "Ange bostadens månadsavgift för att gå vidare."),
-        v.maxValue(10_000, "Månadsavgiften är för hög. Max 10 000 kr/mån."),
-      ),
-    }),
-    v.object({
-      property_type: v.union([v.literal("house"), v.literal("terraced_house"), v.literal("holiday_home")]),
-      operating_cost: v.pipe(
-        v.string("Ange bostadens driftskostnad för att gå vidare."),
-        v.nonEmpty("Ange bostadens driftskostnad för att gå vidare."),
-        v.transform((value) => Number(value)),
-        v.number("Must be a valid number"),
-        v.minValue(1, "Ange bostadens driftskostnad för att gå vidare."),
-        v.maxValue(10_000, "Driftskostnaden är för hög. Max 10 000 kr/mån."),
-      ),
-    }),
-  ]),
-]);
+export const rooms = v.pipe(
+  v.string("Ange hur många rum bostaden har för att gå vidare."),
+  v.nonEmpty("Ange hur många rum bostaden har för att gå vidare."),
+  v.transform((value) => Number(value)),
+  v.number("Must be a valid number."),
+  v.minValue(1, "Du kan som lägst ange 1 rum."),
+  v.maxValue(10, "Du kan som högst ange 10 rum."),
+);
 
-export default schema;
+export const monthly_fee = v.pipe(
+  v.string("Ange bostadens månadsavgift för att gå vidare."),
+  v.nonEmpty("Ange bostadens månadsavgift för att gå vidare."),
+  v.transform((value) => Number(value)),
+  v.number("Must be a valid number"),
+  v.minValue(1, "Ange bostadens månadsavgift för att gå vidare."),
+  v.maxValue(10_000, "Månadsavgiften är för hög. Max 10 000 kr/mån."),
+);
+
+export const operating_cost = v.pipe(
+  v.string("Ange bostadens driftskostnad för att gå vidare."),
+  v.nonEmpty("Ange bostadens driftskostnad för att gå vidare."),
+  v.transform((value) => Number(value)),
+  v.number("Must be a valid number"),
+  v.minValue(1, "Ange bostadens driftskostnad för att gå vidare."),
+  v.maxValue(10_000, "Driftskostnaden är för hög. Max 10 000 kr/mån."),
+);
+
+export const schema = v.object({
+  size,
+  rooms,
+  monthly_fee,
+  operating_cost,
+});
