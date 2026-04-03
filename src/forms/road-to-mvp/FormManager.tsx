e6ac9800b7833b0e5e37e443f3f8a5c0cb918886
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-04-03 15:48:57 +0200

    adding intermediary step

--- src/forms/road-to-mvp/FormManager.tsx
+++ src/forms/road-to-mvp/FormManager.tsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 // Project files
 import Step1 from "./step-1/Step1";
 import Step2 from "./step-2/Step2";
+import Step3 from "./step-3/Step3";
 import Success from "./success/Success";
 
-type Step = "step1" | "step2" | "success";
+type Step = "step1" | "step2" | "step3" | "success";
 
 export default function FormManager() {
   // Local state
@@ -15,7 +16,8 @@ export default function FormManager() {
   return (
     <div>
       {step === "step1" && <Step1 onContinue={() => setStep("step2")} />}
-      {step === "step2" && <Step2 onContinue={() => setStep("success")} />}
+      {step === "step2" && <Step2 onContinue={() => setStep("step3")} />}
+      {step === "step3" && <Step3 onContinue={() => setStep("success")} />}
       {step === "success" && <Success onContinue={() => setStep("step1")} />}
     </div>
   );
--- src/forms/road-to-mvp/step-2/Step2.tsx
+++ src/forms/road-to-mvp/step-2/Step2.tsx
@@ -1,62 +1,32 @@
-// Node modules
-import { Form, useForm } from "@formisch/react";
-
 // Project files
 import Button from "components/button/Button";
 import Icon from "components/icon/Icon";
-import Input from "components/input/Input";
-import InputField from "components/input-field/InputField";
-import Label from "components/label/Label";
-
-import schema from "./schema";
-import "./step-2.css";
 
 interface Props {
   onContinue: () => void;
 }
 
 export default function Step2({ onContinue }: Props) {
-  // Properties
-  const form = useForm({ schema: schema, validate: "blur" });
-
-  // Methods
-  function submitForm() {
-    if (form.isValid) onContinue();
-  }
-
   return (
-    <Form of={form} onSubmit={submitForm} id="step-2" className="soft-background">
+    <div id="step-2" className="soft-background">
       <section className="top">
         <header>
           <a className="link-go-back" href="/">
             <Icon name="arrow-left" /> Tillbaka
           </a>
-          <h4>2. Om bostaden</h4>
+          <h4>1. About the loan</h4>
         </header>
 
-        <InputField form={form} id="size">
-          <Label>Kvadratmeter</Label>
-          <Input type="number" placeholder="0" suffix="kvm" />
-        </InputField>
-
-        <InputField form={form} id="rooms">
-          <Label>Antal rum</Label>
-          <Input type="number" placeholder="0" suffix="st" />
-        </InputField>
-
-        <InputField form={form} id="monthly_fee">
-          <Label>Månadsavgift</Label>
-          <Input type="number" placeholder="0" suffix="kr/mån" />
-        </InputField>
+        <p>Small form goes here</p>
       </section>
 
       <hr />
 
       <section className="bottom">
-        <Button type="submit">
-          Nästa <Icon name="arrow-right" />
+        <Button onClick={onContinue}>
+          Next <Icon name="arrow-right" />
         </Button>
       </section>
-    </Form>
+    </div>
   );
 }
