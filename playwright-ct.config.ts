712dc385ae320f1239514c1d377e659d4f3ae8de
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-04-04 21:28:17 +0200

    ss

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 6 : undefined,
+  workers: process.env.CI ? 8 : undefined,
   reporter: "list",
   use: {
     trace: "on-first-retry",
