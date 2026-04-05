bec9262ee07a1f9149d8f828892b4057fe3df25d
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-04-05 16:04:10 +0200

    refactor step 3

--- src/forms/road-to-mvp/step-3/schema.ts
+++ src/forms/road-to-mvp/step-3/schema.ts
@@ -1,7 +1,7 @@
 // Node modules
 import * as v from "valibot";
 
-export const size = v.pipe(
+const size = v.pipe(
   v.string("Ange bostadens yta för att gå vidare."),
   v.nonEmpty("Ange bostadens yta för att gå vidare."),
   v.transform((value) => Number(value)),
@@ -10,7 +10,7 @@ export const size = v.pipe(
   v.maxValue(2_000, "Boytan är för stor. Max 2000 kvm."),
 );
 
-export const rooms = v.pipe(
+const rooms = v.pipe(
   v.string("Ange hur många rum bostaden har för att gå vidare."),
   v.nonEmpty("Ange hur många rum bostaden har för att gå vidare."),
   v.transform((value) => Number(value)),
@@ -19,7 +19,7 @@ export const rooms = v.pipe(
   v.maxValue(10, "Du kan som högst ange 10 rum."),
 );
 
-export const monthly_fee = v.pipe(
+const monthly_fee = v.pipe(
   v.string("Ange bostadens månadsavgift för att gå vidare."),
   v.nonEmpty("Ange bostadens månadsavgift för att gå vidare."),
   v.transform((value) => Number(value)),
@@ -28,7 +28,7 @@ export const monthly_fee = v.pipe(
   v.maxValue(10_000, "Månadsavgiften är för hög. Max 10 000 kr/mån."),
 );
 
-export const operating_cost = v.pipe(
+const operating_cost = v.pipe(
   v.string("Ange bostadens driftskostnad för att gå vidare."),
   v.nonEmpty("Ange bostadens driftskostnad för att gå vidare."),
   v.transform((value) => Number(value)),
@@ -36,3 +36,11 @@ export const operating_cost = v.pipe(
   v.minValue(1, "Ange bostadens driftskostnad för att gå vidare."),
   v.maxValue(10_000, "Driftskostnaden är för hög. Max 10 000 kr/mån."),
 );
+
+export default function buildSchema(propertyType: string) {
+  if (propertyType === "apartment") {
+    return v.object({ size, rooms, monthly_fee });
+  } else {
+    return v.object({ size, rooms, operating_cost });
+  }
+}
