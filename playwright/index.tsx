67cbd121bcab5e5920946456334411f65dc08db9
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-04-05 14:48:27 +0200

    adding path resolution to Playwright

--- playwright/index.tsx
+++ playwright/index.tsx
@@ -1,3 +1,3 @@
 // Import styles, initialize component theme here.
-import "../src/styles/style.css";
+import "../src/styles/style.css"; /* This file is not compiled by Playwright CT config, thus, requires the full path. */
 import "./center-component.css";
