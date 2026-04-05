1992a9c3d51c167a9582af7e35fbbaf1106c58c8
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-04-05 21:46:39 +0200

    making pw run with 8 cores

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 0,
-  workers: process.env.CI ? 8 : undefined,
+  workers: 8,
   reporter: "list",
   use: {
     trace: "on-first-retry",
