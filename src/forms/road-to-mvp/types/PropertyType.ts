b1bd39101def48999bea627215b82971a3d53604
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-04-10 23:54:23 +0200

    small update

--- src/forms/road-to-mvp/types/PropertyType.ts
+++ src/forms/road-to-mvp/types/PropertyType.ts
@@ -1,3 +1,4 @@
+// Project files
 import { apartment, holidayHome, terracedHouse, house } from "../helpers/constants";
 
 export type PropertyType = typeof apartment | typeof holidayHome | typeof terracedHouse | typeof house | undefined;
