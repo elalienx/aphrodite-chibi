dd67f4b88d340042e10e99f68bd635103c43062a
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-04-18 22:02:38 +0200

    sorted fields

--- src/forms/road-to-mvp/-step-2/schema.ts
+++ src/forms/road-to-mvp/-step-2/schema.ts
@@ -2,24 +2,6 @@
 import * as v from "valibot";
 import type { PropertyType } from "../types/PropertyType";
 
-const size = v.pipe(
-  v.string("Ange bostadens yta för att gå vidare."),
-  v.nonEmpty("Ange bostadens yta för att gå vidare."),
-  v.transform((value) => Number(value)),
-  v.number("Must be a valid number."),
-  v.minValue(1, "Boytan är för liten."),
-  v.maxValue(2_000, "Boytan är för stor. Max 2000 kvm."),
-);
-
-const rooms = v.pipe(
-  v.string("Ange hur många rum bostaden har för att gå vidare."),
-  v.nonEmpty("Ange hur många rum bostaden har för att gå vidare."),
-  v.transform((value) => Number(value)),
-  v.number("Must be a valid number."),
-  v.minValue(1, "Du kan som lägst ange 1 rum."),
-  v.maxValue(10, "Du kan som högst ange 10 rum."),
-);
-
 const monthly_fee = v.pipe(
   v.string("Ange bostadens månadsavgift för att gå vidare."),
   v.nonEmpty("Ange bostadens månadsavgift för att gå vidare."),
@@ -38,9 +20,27 @@ const operating_cost = v.pipe(
   v.maxValue(10_000, "Driftskostnaden är för hög. Max 10 000 kr/mån."),
 );
 
+const rooms = v.pipe(
+  v.string("Ange hur många rum bostaden har för att gå vidare."),
+  v.nonEmpty("Ange hur många rum bostaden har för att gå vidare."),
+  v.transform((value) => Number(value)),
+  v.number("Must be a valid number."),
+  v.minValue(1, "Du kan som lägst ange 1 rum."),
+  v.maxValue(10, "Du kan som högst ange 10 rum."),
+);
+
 const tenant_ownership = v.pipe(
   v.string("Say either yes or no."),
   v.transform((value) => value === "true"), // converts to boolean
+);
+
+const size = v.pipe(
+  v.string("Ange bostadens yta för att gå vidare."),
+  v.nonEmpty("Ange bostadens yta för att gå vidare."),
+  v.transform((value) => Number(value)),
+  v.number("Must be a valid number."),
+  v.minValue(1, "Boytan är för liten."),
+  v.maxValue(2_000, "Boytan är för stor. Max 2000 kvm."),
 );
 
 const apartmentSchema = v.object({ tenant_ownership, size, rooms, monthly_fee });
