adda63b2923526b625e03bb967cbe6b7d288e634
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-04-18 15:55:37 +0200

    adding boilerplate

---
+++ src/forms/example-3/FormPage.tsx
@@ -0,0 +1,55 @@
+// Node modules
+import { Form, useForm } from "@formisch/react";
+import * as v from "valibot";
+
+// Project files
+import Button from "components/button/Button";
+import Label from "components/label/Label";
+import RadioGroup from "components/radio-group/RadioGroup";
+import RadioOption from "components/radio-option/RadioOption";
+
+const schema = v.object({
+  likes_beer: v.pipe(v.string("Say either yes or no.")),
+  likes_guiness: v.pipe(
+    v.string("Say either yes or no."),
+    v.transform((value) => value === "true"), // converts to boolean
+  ),
+});
+
+export default function FormPage() {
+  // Local state
+  const form = useForm({ schema: schema, validate: "blur", revalidate: "blur" });
+
+  // Methods
+  function submitForm() {
+    if (form.isValid) alert("Success");
+  }
+
+  return (
+    <Form of={form} onSubmit={submitForm} className="soft-background">
+      <section className="top">
+        <h4>Dynamic field validation</h4>
+
+        <RadioGroup form={form} id="likes_beer">
+          <Label>Do you like beer?</Label>
+          <RadioOption value="yes">Yes</RadioOption>
+          <RadioOption value="no">No</RadioOption>
+        </RadioGroup>
+
+        <RadioGroup form={form} id="likes_guiness">
+          <Label> Do you like Guiness?</Label>
+          <RadioOption value={true}>Yes</RadioOption>
+          <RadioOption value={false}>No</RadioOption>
+        </RadioGroup>
+      </section>
+
+      <hr />
+
+      <section className="bottom" style={{ textAlign: "center" }}>
+        <Button type="submit">Submit</Button>
+        <br />
+        <small>(Text to clean Playwright selector)</small>
+      </section>
+    </Form>
+  );
+}
