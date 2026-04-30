a64ffb27e1ffcffc54ef555e79c6649d16d0840a
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-05-01 01:15:40 +0200

    added documentation

--- src/components/input/helpers/InputState.ts
+++ src/components/input/helpers/InputState.ts
@@ -1,1 +1,7 @@
+/** The states a Lendo input field can be, each one has specific color:
+ * * default: grey.
+ * * focus: blue.
+ * * error: red.
+ * * success: green.
+ */
 export type InputState = "default" | "focus" | "error" | "success";
