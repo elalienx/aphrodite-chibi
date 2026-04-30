79319c88b5acc7642c5bffb16ad8f0313d26dcc1
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-04-30 16:38:33 +0200

    refactor input on change

--- src/components/input/helpers/parseDigits.ts
+++ src/components/input/helpers/parseDigits.ts
@@ -1,7 +1,7 @@
 /**
- * HTML default `<input type="number"/>` accepts the characters `+`, `-`, and `e` (for math exponents).
+ * HTML `<input type="number"/>` accepts the characters `+`, `-`, and `e` (math exponent).
  *
- * This method cleans the user input to force only the digits `0` to `9`.
+ * This method cleans the user input to only allow the digits `0` to `9`.
  */
 export default function parseDigits(value: string): string {
   const nonDigits: RegExp = /\D/g;
