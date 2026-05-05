8284715d443d16f87b6e64f314ce8b4f961d584f
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-05-05 14:05:23 +0200

    chore: Polish Step 2 validation messages

--- src/forms/road-to-mvp/-step-2/schema.ts
+++ src/forms/road-to-mvp/-step-2/schema.ts
@@ -6,33 +6,33 @@ import type { PropertyType } from "../types/PropertyType";
 const monthly_fee = v.pipe(
   v.string("Ange bostadens månadsavgift för att gå vidare."),
   v.nonEmpty("Ange bostadens månadsavgift för att gå vidare."),
-  v.toNumber("Must be a valid number"),
+  v.toNumber("Du kan bara använda dig utav siffror."),
   v.minValue(1, "Ange bostadens månadsavgift för att gå vidare."),
-  v.maxValue(10_000, "Månadsavgiften är för hög. Max 10 000 kr/mån."),
+  v.maxValue(100_000, "Månadsavgiften är för hög. Du kan som högst ange en avgift på 100 000 kr/mån."),
 );
 
 const operating_cost = v.pipe(
   v.string("Ange bostadens driftskostnad för att gå vidare."),
   v.nonEmpty("Ange bostadens driftskostnad för att gå vidare."),
-  v.toNumber("Must be a valid number"),
+  v.toNumber("'Du kan bara använda dig utav siffror."),
   v.minValue(1, "Ange bostadens driftskostnad för att gå vidare."),
-  v.maxValue(10_000, "Driftskostnaden är för hög. Max 10 000 kr/mån."),
+  v.maxValue(100_000, "Månadsavgiften är för hög. Du kan som högst ange en avgift på 100 000 kr/mån."),
 );
 
 const rooms = v.pipe(
   v.string("Ange hur många rum bostaden har för att gå vidare."),
   v.nonEmpty("Ange hur många rum bostaden har för att gå vidare."),
-  v.toNumber("Must be a valid number."),
-  v.minValue(1, "Du kan som lägst ange 1 rum."),
-  v.maxValue(10, "Du kan som högst ange 10 rum."),
+  v.toNumber("Du kan bara använda dig utav siffror."),
+  v.minValue(1, "Du har angivit för få rum. Du kan som lägst ange 1 rum."),
+  v.maxValue(10, "Du har angivit för många rum. Du kan som högst ange 10 rum."),
 );
 
 const size = v.pipe(
-  v.string("Ange bostadens yta för att gå vidare."),
-  v.nonEmpty("Ange bostadens yta för att gå vidare."),
-  v.toNumber("Must be a valid number."),
-  v.minValue(1, "Boytan är för liten."),
-  v.maxValue(2_000, "Boytan är för stor. Max 2000 kvm."),
+  v.string("Ange bostadens boyta för att gå vidare."),
+  v.nonEmpty("Ange bostadens boyta för att gå vidare."),
+  v.toNumber("Du kan bara använda dig utav siffror."),
+  v.minValue(1, "Ange bostadens boyta för att gå vidare."),
+  v.maxValue(2_000, "Boytan är för stor. Du kan som störst ange en boyta på 2 000 kvm."),
 );
 
 // Variants (for tenancy agreement)
