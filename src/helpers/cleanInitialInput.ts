7827d5aebe652610eb35f86d43dba60657130e04
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-05-09 23:22:57 +0200

    refactor: Clean up initial input method

--- src/helpers/cleanInitialInput.ts
+++ src/helpers/cleanInitialInput.ts
@@ -1,5 +1,18 @@
+/**
+ * Formisch use strings by default just as native HTML <input>.
+ * This creates a conflict because Valibot schemas expect numbers.
+ *
+ * This method converts initial numeric values to strings to avoid errors.
+ * Values are converted back to numbers during form validation.
+ */
 export default function cleanInitialInput(values: object): object {
-  return Object.fromEntries(
-    Object.entries(values).map(([key, value]) => [key, typeof value === "number" ? String(value) : value]),
-  );
+  const entries = Object.entries(values);
+  const cleanedEntries = entries.map(([key, value]) => {
+    const isNumber = typeof value === "number";
+    const safeValue = isNumber ? String(value) : value;
+
+    return [key, safeValue];
+  });
+
+  return Object.fromEntries(cleanedEntries);
 }
