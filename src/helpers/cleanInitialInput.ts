8444da88a7a849c6c7866c1fb3c6b63cf793bc03
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-05-11 11:13:18 +0200

    improved documentation

--- src/helpers/cleanInitialInput.ts
+++ src/helpers/cleanInitialInput.ts
@@ -1,9 +1,13 @@
 /**
- * Formisch use strings by default just as native HTML <input>.
+ * Formisch use strings by default just as native HTML `<input>`.
  * This creates a conflict because Valibot schemas expect numbers.
  *
  * This method converts initial numeric values to strings to avoid errors.
  * Values are converted back to numbers during form validation.
+ *
+ * @param {object} values - The initial form values object to be processed.
+ * @param {boolean} [treatZeroAsEmpty=false] - If true, treats the number 0 or string "0" as an empty string.
+ * @returns {object} A new object with numeric values converted to strings.
  */
 export default function cleanInitialInput(values: object, treatZeroAsEmpty = false): object {
   const entries = Object.entries(values);
