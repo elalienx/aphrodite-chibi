e9c950dada2de52a861f00260fc395a725641141
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-05-18 11:43:05 +0200

    converting to React component

---
+++ src/components/select/Select.tsx
@@ -0,0 +1,19 @@
+// Project files
+import "./select.css";
+
+export default function Select() {
+  return (
+    <>
+      <button className="select-trigger" popoverTarget="select-list" style={{ anchorName: "--select-trigger" }}>
+        Please choose an option
+      </button>
+
+      <div id="select-list" popover="auto" style={{ positionAnchor: "--select-trigger" }}>
+        <button className="select-option">House</button>
+        <button className="select-option">Apartment</button>
+        <button className="select-option">Terraced house</button>
+        <button className="select-option">Holiday home</button>
+      </div>
+    </>
+  );
+}
