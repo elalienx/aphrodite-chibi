bdfa2a8eabe66ff8760408d940776c3dc02137ff
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-05-23 12:56:17 +0200

    small refactor

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from "react";
 
 // Project files
 import Icon from "components/icon/Icon";
+import "./select.css";
 
 interface Props {
   id?: string;
@@ -14,17 +15,11 @@ interface Props {
 
 export default function Select({ id, anchorId, children, listId, activeOptionText }: Props) {
   // Properties
-  const triggerId = `trigger-${id}`;
+  const selectId = `trigger-${id}`;
   const textToDisplay = activeOptionText ? activeOptionText : children;
 
   return (
-    <button
-      id={triggerId}
-      type="button"
-      className="select-trigger"
-      popoverTarget={listId}
-      style={{ anchorName: anchorId }}
-    >
+    <button id={selectId} type="button" className="select" popoverTarget={listId} style={{ anchorName: anchorId }}>
       {textToDisplay}
       <Icon name={"chevron-down"} />
     </button>
