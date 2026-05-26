cb91243e2a2582a07ed3c80f805488fc1eff84fd
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-05-26 23:14:32 +0200

    fix: Made Tooltip tests stable

--- playwright/components/Tooltip.spec.tsx
+++ playwright/components/Tooltip.spec.tsx
@@ -12,7 +12,6 @@ const tooltipText1 = "Click me for more info";
 const tooltipText2 = "Write both your first and last name.";
 const tooltipText3 = "You can see yes if you like Cider as well.";
 const tooltipText4 = "About Guiness!";
-const tooltipVisualThreshold = 0.1;
 
 let cleanUpText: Locator;
 let component: MountResult;
@@ -36,7 +35,7 @@ test.afterEach(async () => {
   await expect(cleanUpText).toBeVisible();
 
   // Only run visual regression locally
-  if (!process.env.CI) await expect(component).toHaveScreenshot({ threshold: tooltipVisualThreshold });
+  if (!process.env.CI) await expect(component).toHaveScreenshot();
 });
 
 test("1. Clicking on a tooltip does not trigger a form submission", async () => {
