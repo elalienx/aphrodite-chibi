5adffc03e88c4c5226e814de439bdc06d0411928
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-05-27 21:34:22 +0200

    refactor: Reorganize Checkbox tests

--- playwright/components/Checkox.spec.tsx
+++ playwright/components/Checkox.spec.tsx
@@ -1,68 +1,58 @@
 // Node modules
-// @ts-ignore
-import type { Locator } from "@playwright/test";
 import { test, expect, type MountResult } from "@playwright/experimental-ct-react";
 
 // Project files
 import FormPage from "forms/example-checkbox/FormPage";
 
-let component: MountResult;
-let checkbox1: Locator;
-let checkbox2: Locator;
-let cleanUpText: Locator;
-let onValid: Locator;
-let onError: Locator;
-let submitButton: Locator;
-
-test.beforeEach(async ({ mount }) => {
-  component = await mount(<FormPage />);
-  cleanUpText = component.getByText("Text to clean Playwright selector");
-  checkbox1 = component.getByRole("checkbox", { name: "Do you accept our terms and conditions?" });
-  checkbox2 = component.getByRole("checkbox", { name: "I certify that I am NOT a politically exposed person (PEP)" });
-  onValid = component.getByText("The form passed the validation");
-  onError = component.getByText("The form failed the validation");
-  submitButton = component.getByRole("button", { name: "Submit" });
-});
+const submit = "Submit";
+const item1 = "Do you accept our terms and conditions?";
+const item2 = "I certify that I am NOT a politically exposed person (PEP)";
+const textSuccess = "The form passed the validation";
+const textFailure = "The form failed the validation";
+let form: MountResult;
 
 test.afterEach(async () => {
-  await expect(cleanUpText).toBeVisible();
-
   // Only run visual regression locally
-  if (!process.env.CI) await expect(component).toHaveScreenshot();
+  if (!process.env.CI) await expect(form).toHaveScreenshot();
 });
 
-test("1. Should submit as soon as you press the submit button", async () => {
-  // Act
-  await submitButton.click();
-
-  // Assert
-  await expect(checkbox1).not.toBeChecked();
-  await expect(checkbox2).toBeChecked();
-  await expect(onValid).toBeVisible();
-});
-
-test("2. Should submit if you check the first checkbox", async () => {
+test("1. Should submit as soon as you press the submit button", async ({ mount }) => {
   // Arrange
-  await checkbox1.check();
+  form = await mount(<FormPage />);
 
   // Act
-  await submitButton.click();
+  await form.getByRole("button", { name: submit }).click();
 
   // Assert
-  await expect(checkbox1).toBeChecked();
-  await expect(checkbox2).toBeChecked();
-  await expect(onValid).toBeVisible();
+  await expect(form.getByRole("checkbox", { name: item1 })).not.toBeChecked();
+  await expect(form.getByRole("checkbox", { name: item2 })).toBeChecked();
+  await expect(form.getByText(textSuccess)).toBeVisible();
 });
 
-test("3. Should fail if you uncheck the second checkbos due to the Political warning", async () => {
+test("2. Should submit if you check the first checkbox", async ({ mount }) => {
   // Arrange
-  await checkbox2.uncheck();
+  form = await mount(<FormPage />);
 
   // Act
-  await submitButton.click();
+  await form.getByRole("checkbox", { name: item1 }).check();
+  await form.getByRole("button", { name: submit }).click();
 
   // Assert
-  await expect(checkbox1).not.toBeChecked();
-  await expect(checkbox2).not.toBeChecked();
-  await expect(onError).toBeVisible();
+  await expect(form.getByRole("checkbox", { name: item1 })).toBeChecked();
+  await expect(form.getByRole("checkbox", { name: item2 })).toBeChecked();
+  await expect(form.getByText(textSuccess)).toBeVisible();
+});
+
+test("3. Should fail if you uncheck the second checkbos due to the Political warning", async ({ mount }) => {
+  // Arrange
+  form = await mount(<FormPage />);
+
+  // Act
+  await form.getByRole("checkbox", { name: item2 }).uncheck();
+  await form.getByRole("button", { name: submit }).click();
+
+  // Assert
+  await expect(form.getByRole("checkbox", { name: item1 })).not.toBeChecked();
+  await expect(form.getByRole("checkbox", { name: item2 })).not.toBeChecked();
+  await expect(form.getByText(textFailure)).toBeVisible();
 });
