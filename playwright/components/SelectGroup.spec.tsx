917c87e0b61915ff63b83a4c5e7398cdd6ed65ed
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-05-27 22:43:58 +0200

    refactor select group tests

--- playwright/components/SelectGroup.spec.tsx
+++ playwright/components/SelectGroup.spec.tsx
@@ -1,115 +1,99 @@
 // Node modules
-// @ts-ignore
-import type { Locator } from "@playwright/test";
 import { test, expect, type MountResult } from "@playwright/experimental-ct-react";
 
 // Project files
 import FormPage from "forms/example-select-group/FormPage";
 
-let component: MountResult;
-let cleanUpText: Locator;
-let error1: Locator;
-let error2: Locator;
-let option1A: Locator;
-let option2A: Locator;
-let option2B: Locator;
-let output1: Locator;
-let output2A: Locator;
-let output2B: Locator;
-let select1: Locator;
-let select2: Locator;
-let submitButton: Locator;
+const error1 = "Choose one game developer company.";
+const error2 = "Choose one accessory.";
+const item1 = "Choose a developer";
+const item1Option1 = "Capcom";
+const item2 = "Choose an accessory";
+const item2Option1 = "Arcade stick";
+const item2Option2 = "Zapper";
+const output1 = "Select 1 internal value: capcom";
+const output2A = "Select 2 internal value: 0";
+const output2B = "Select 2 internal value: 5";
+const submit = "Submit";
+const textCleanup = "Text to clean Playwright selector";
+let form: MountResult;
 
 test.beforeEach(async ({ mount }) => {
-  component = await mount(<FormPage />);
-  submitButton = component.getByRole("button", { name: "Submit" });
-  cleanUpText = component.getByText("Text to clean Playwright selector");
-
-  // Select 1
-  select1 = component.getByRole("button", { name: "Choose a developer" });
-  option1A = component.getByText("Capcom");
-  error1 = component.getByText("Choose one game developer company.");
-  output1 = component.getByText("Select 1 internal value: capcom");
-
-  // Select 2
-  select2 = component.getByRole("button", { name: "Choose an accessory" });
-  option2A = component.getByText("Arcade stick");
-  option2B = component.getByText("Zapper"); // last item
-  error2 = component.getByText("Choose one accessory.");
-  output2A = component.getByText("Select 2 internal value: 0");
-  output2B = component.getByText("Select 2 internal value: 5");
+  // Arrange
+  form = await mount(<FormPage />);
 });
 
 test.afterEach(async () => {
-  await expect(cleanUpText).toBeVisible();
+  // Assert
+  await expect(form.getByText(textCleanup)).toBeVisible();
 
   // Only run visual regression locally
-  if (!process.env.CI) await expect(component).toHaveScreenshot();
+  if (!process.env.CI) await expect(form).toHaveScreenshot();
 });
 
 test("1. Should show error state when submitting empty form", async () => {
   // Act
-  await submitButton.click();
+  await form.getByRole("button", { name: submit }).click();
 
   // Assert
-  await expect(error1).toBeVisible();
-  await expect(error2).toBeVisible();
+  await expect(form.getByText(error1)).toBeVisible();
+  await expect(form.getByText(error2)).toBeVisible();
 });
 
 test("2. Should not submit when choosing only 1 option", async () => {
   // Arrange
   await test.step("First select", async () => {
-    await select1.click();
-    await option1A.click();
+    await form.getByRole("button", { name: item1 }).click();
+    await form.getByText(item1Option1).click();
   });
 
   // Act
-  await submitButton.click();
+  await form.getByRole("button", { name: submit }).click();
 
   // Assert
-  await expect(output1).toBeVisible();
-  await expect(error1).not.toBeVisible();
-  await expect(error2).toBeVisible();
+  await expect(form.getByText(output1)).toBeVisible();
+  await expect(form.getByText(error1)).not.toBeVisible();
+  await expect(form.getByText(error2)).toBeVisible();
 });
 
 test("3. Should be able to submit", async () => {
   // Arrange
   await test.step("First select", async () => {
-    await select1.click();
-    await option1A.click();
+    await form.getByRole("button", { name: item1 }).click();
+    await form.getByText(item1Option1).click();
   });
   await test.step("Second select", async () => {
-    await select2.click();
-    await option2A.click();
+    await form.getByRole("button", { name: item2 }).click();
+    await form.getByText(item2Option1).click();
   });
 
   // Act
-  await submitButton.click();
+  await form.getByRole("button", { name: submit }).click();
 
   // Assert
-  await expect(output1).toBeVisible();
-  await expect(output2A).toBeVisible();
-  await expect(error1).not.toBeVisible();
-  await expect(error2).not.toBeVisible();
+  await expect(form.getByText(output1)).toBeVisible();
+  await expect(form.getByText(output2A)).toBeVisible();
+  await expect(form.getByText(error1)).not.toBeVisible();
+  await expect(form.getByText(error2)).not.toBeVisible();
 });
 
 test("4. Should be able to submit a long list", async () => {
   // Arrange
   await test.step("First select", async () => {
-    await select1.click();
-    await option1A.click();
+    await form.getByRole("button", { name: item1 }).click();
+    await form.getByText(item1Option1).click();
   });
   await test.step("Second select", async () => {
-    await select2.click();
-    await option2B.click();
+    await form.getByRole("button", { name: item2 }).click();
+    await form.getByText(item2Option2).click();
   });
 
   // Act
-  await submitButton.click();
+  await form.getByRole("button", { name: submit }).click();
 
   // Assert
-  await expect(output1).toBeVisible();
-  await expect(output2B).toBeVisible();
-  await expect(error1).not.toBeVisible();
-  await expect(error2).not.toBeVisible();
+  await expect(form.getByText(output1)).toBeVisible();
+  await expect(form.getByText(output2B)).toBeVisible();
+  await expect(form.getByText(error1)).not.toBeVisible();
+  await expect(form.getByText(error2)).not.toBeVisible();
 });
