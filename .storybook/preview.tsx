be50210fb21e1ecb3149b168fe2fd55c3a8ee62a
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-05-30 12:25:54 +0200

    finish first component

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,7 @@ import { definePreview } from "@storybook/react-vite";
 
 // Project files
 import "../src/styles/style.css";
+import "./center-components.css";
 
 export default definePreview({
   addons: [],
--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -3,16 +3,33 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 
 // Project files
 import Button from "./Button";
+import Icon from "../icon/Icon";
 
 type Story = StoryObj<typeof Button>;
 
+// Properties
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
 };
 
+// Methods
+function onClick() {
+  alert("Miku");
+}
+
+// Storties
 export const Primary: Story = {
-  render: () => <Button onClick={() => alert("Miku")}>Hello</Button>,
+  render: () => <Button onClick={onClick}>Hello</Button>,
+};
+
+export const PrimaryWithIcon: Story = {
+  name: "Primary with icon",
+  render: () => (
+    <Button onClick={onClick}>
+      Hello <Icon name="arrow-right" />
+    </Button>
+  ),
 };
 
 export default meta;
