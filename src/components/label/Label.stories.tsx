90826651f4d17ee1dfffd59f319d2224e4924fa9
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-06-03 10:54:16 +0200

    refactor

--- src/components/label/Label.stories.tsx
+++ src/components/label/Label.stories.tsx
@@ -4,12 +4,26 @@ import Label from "components/label/Label";
 
 // Metadata
 const meta = preview.meta({
-  title: "Components/Label",
+  title: "Form fields/Label",
   component: Label,
 });
 
 // Stories
-export const Primary = meta.story({
-  name: "Primary",
-  render: () => <Label id="something">Hola mundo</Label>,
+export const Story1 = meta.story({
+  name: "Label",
+  render: () => <Label id="email">Write your email</Label>,
+});
+
+export const Story2 = meta.story({
+  name: "Label with tooltip",
+  render: () => (
+    <Label id="email" hint="we ask for your email so you can login again in the future.">
+      Write your email
+    </Label>
+  ),
+});
+
+export const Story3 = meta.story({
+  name: "Label with error",
+  render: () => <Label>Write your email</Label>,
 });
