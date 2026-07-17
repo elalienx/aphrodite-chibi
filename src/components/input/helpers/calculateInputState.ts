15a23aadac0769f66e5cc436e405fdd3e4955c95
Eduardo Alvarez <eduardo.alvarez.nowak@vend.com>
2026-07-17 23:06:17 +0200

    rename isFocused in helper method

--- src/components/input/helpers/calculateInputState.ts
+++ src/components/input/helpers/calculateInputState.ts
@@ -16,22 +16,22 @@ export default function calculateInputState(
   form: FormStore,
   field: FieldStore,
   inputState: InputState,
-  inputIsFocused: boolean,
+  isFocused: boolean,
 ): InputState {
   // Show error after form submission
   if (form.isSubmitted && !field.isValid) return "error";
 
   // If the field already had an error, keep it when focusing again
-  if (inputState === "error" && inputIsFocused) return "error";
+  if (inputState === "error" && isFocused) return "error";
 
   // If the field already had an error, keep it if user clears the input
   if (inputState === "error" && field.input === "") return "error";
 
   // If the field already had a success, keep it when focusing again
-  if (inputState === "success" && inputIsFocused) return "success";
+  if (inputState === "success" && isFocused) return "success";
 
   // While editing a fresh field, stay in focus state
-  if (inputIsFocused) return "focus";
+  if (isFocused) return "focus";
 
   // Default before interaction
   if (!field.isDirty) return "default";
