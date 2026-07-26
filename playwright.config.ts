57b9fef8bf4daf6007a00329356102efbcf704b5
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-07-26 15:54:10 +0200

    moved test folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,12 @@
 // Node modules
 import { defineConfig, devices } from "@playwright/test";
 
+// Properties
+const TEST_FOLDER = "./playwright";
 const GALLERY_URL = "http://localhost:3100/playwright/index.html";
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: TEST_FOLDER,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -12,7 +14,7 @@ export default defineConfig({
   projects: [
     {
       name: "components",
-      testDir: "./tests",
+      testDir: TEST_FOLDER,
       use: {
         ...devices["Desktop Chrome"],
         baseURL: GALLERY_URL,
