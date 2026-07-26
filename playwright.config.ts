22c07ab167972a4709427af828040ec6429b5fed
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-07-26 23:23:17 +0200

    chore: Clean up Playwright commands

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "pnpm gallery:build && pnpm gallery:preview",
+    command: "pnpm playwright:build && pnpm playwright:preview",
     url: GALLERY_URL,
     reuseExistingServer: !process.env.CI,
   },
